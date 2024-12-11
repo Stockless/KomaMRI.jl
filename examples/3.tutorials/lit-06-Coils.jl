2e38e38a5d5fc305077ddb157952bcab2c36842f
Stockless <mar.stockle@gmail.com>
2024-12-11 10:13:05 -0300

    test with 4 coils

--- examples/3.tutorials/lit-06-Coils.jl
+++ examples/3.tutorials/lit-06-Coils.jl
@@ -3,9 +3,11 @@
 using KomaMRI, MAT # hide
 sys = Scanner() # hide
 obj = brain_phantom2D()
-coil_sens1 = exp.(-π * (((obj.x) .+ 0.1) .^ 2 / 0.01) .+ ((obj.y) .^ 2 / 0.1))
-coil_sens2 = exp.(-π * (((obj.x) .- 0.1) .^ 2 / 0.01) .+ ((obj.y) .^ 2 / 0.1))
-coil_sens = hcat(coil_sens1, coil_sens2)
+coil_sens1 = exp.(-π * (((obj.x) .+ 0.1) .^ 2 / 0.01) .+ ((obj.y) .^ 2 / 0.01))
+coil_sens2 = exp.(-π * (((obj.x) .- 0.1) .^ 2 / 0.01) .+ ((obj.y) .^ 2 / 0.01))
+coil_sens3 = exp.(-π * ((obj.x) .^ 2 / 0.1) .+ (((obj.y) .+ 0.1) .^ 2 / 0.01))/60
+coil_sens4 = exp.(-π * ((obj.x) .^ 2 / 0.1) .+ (((obj.y) .- 0.1) .^ 2 / 0.01))/60
+coil_sens = hcat(coil_sens1, coil_sens2, coil_sens3, coil_sens4)
 obj.coil_sens = coil_sens
 seq_file = joinpath(
     dirname(pathof(KomaMRI)),
@@ -23,6 +25,10 @@ reconParams = Dict{Symbol,Any}(:reco => "direct", :reconSize => (Nx, Ny)) # hide
 image = reconstruction(acq, reconParams) # hide
 slice_abs1 = abs.(image[:, :, 1, 1, 1, 1]) # hide
 slice_abs2 = abs.(image[:, :, 1, 1, 2, 1]) # hide
+slice_abs3 = abs.(image[:, :, 1, 1, 3, 1]) # hide
+slice_abs4 = abs.(image[:, :, 1, 1, 4, 1]) # hide
 p3 = plot_image(slice_abs1; height=400)
 p4 = plot_image(slice_abs2; height=400) # hide
-[p3 p4]
+p5 = plot_image(slice_abs3; height=400)
+p6 = plot_image(slice_abs4; height=400) # hide
+[p3 p4 p5 p6]
