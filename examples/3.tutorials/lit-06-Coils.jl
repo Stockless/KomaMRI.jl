b6c9ee295e9db024c8df92610d79f77d0daa9172
Stockless <mar.stockle@gmail.com>
2024-12-12 10:41:57 -0300

    image reconstruction

--- examples/3.tutorials/lit-06-Coils.jl
+++ examples/3.tutorials/lit-06-Coils.jl
@@ -1,6 +1,6 @@
 # # Experimental: Simulating with realistic coils
 
-using KomaMRI, MAT # hide
+using KomaMRI, MAT, MRIReco, MRIReco.RegularizedLeastSquares, Interpolations # hide
 sys = Scanner() # hide
 obj = brain_phantom2D()
 coil_sens1 = exp.(-π * (((obj.x) .+ 0.1) .^ 2 / 0.01) .+ ((obj.y) .^ 2 / 0.01))
@@ -21,14 +21,36 @@ raw = simulate(obj, seq, sys) # hide
 acq = AcquisitionData(raw) # hide
 acq.traj[1].circular = false # hide
 Nx, Ny = raw.params["reconSize"][1:2] # hide
-reconParams = Dict{Symbol,Any}(:reco => "direct", :reconSize => (Nx, Ny)) # hide
-image = reconstruction(acq, reconParams) # hide
-slice_abs1 = abs.(image[:, :, 1, 1, 1, 1]) # hide
-slice_abs2 = abs.(image[:, :, 1, 1, 2, 1]) # hide
-slice_abs3 = abs.(image[:, :, 1, 1, 3, 1]) # hide
-slice_abs4 = abs.(image[:, :, 1, 1, 4, 1]) # hide
-p3 = plot_image(slice_abs1; height=400)
-p4 = plot_image(slice_abs2; height=400) # hide
-p5 = plot_image(slice_abs3; height=400)
-p6 = plot_image(slice_abs4; height=400) # hide
-[p3 p4 p5 p6]
+# reco parameters
+params = Dict{Symbol, Any}()
+params[:reco] = "multiCoil"
+params[:reconSize] = (Nx,Ny)
+params[:reg] = L2Regularization(1.e-3)       # regularization
+params[:iterations] = 40
+params[:solver] = CGNR
+
+# Coil sensitivities interpolated to reconstruction points
+FOV = 230e-3
+xq = range(-FOV/2, FOV/2, Nx)
+yq = range(-FOV/2, FOV/2, Ny)
+coil_sens1 = exp.(-π * (((xq) .+ 0.1) .^ 2 / 0.01) .+ ((yq') .^ 2 / 0.01))
+coil_sens2 = exp.(-π * (((xq) .- 0.1) .^ 2 / 0.01) .+ ((yq') .^ 2 / 0.01))
+coil_sens3 = exp.(-π * ((xq) .^ 2 / 0.1) .+ (((yq') .+ 0.1) .^ 2 / 0.01))/60
+coil_sens4 = exp.(-π * ((xq) .^ 2 / 0.1) .+ (((yq') .- 0.1) .^ 2 / 0.01))/60
+coil_sens_recon = Float32.([coil_sens1[:] coil_sens2[:] coil_sens3[:] coil_sens4[:]])
+params[:senseMaps] = reshape(complex.(coil_sens_recon), Nx, Ny, 1, size(coil_sens_recon, 2))
+
+# do reconstruction
+Ireco = reconstruction(acq, params)
+plot_image(abs.(Ireco.data[:, :]))
+#reconParams = Dict{Symbol,Any}(:reco => "direct", :reconSize => (Nx, Ny)) # hide
+#image = reconstruction(acq, reconParams) # hide
+#slice_abs1 = abs.(image[:, :, 1, 1, 1, 1]) # hide
+#slice_abs2 = abs.(image[:, :, 1, 1, 2, 1]) # hide
+#slice_abs3 = abs.(image[:, :, 1, 1, 3, 1]) # hide
+#slice_abs4 = abs.(image[:, :, 1, 1, 4, 1]) # hide
+#p3 = plot_image(slice_abs1; height=400)
+#p4 = plot_image(slice_abs2; height=400) # hide
+#p5 = plot_image(slice_abs3; height=400)
+#p6 = plot_image(slice_abs4; height=400) # hide
+#[p3 p4 p5 p6]
